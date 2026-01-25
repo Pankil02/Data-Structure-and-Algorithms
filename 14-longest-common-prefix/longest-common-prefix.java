b3394fc3406a79dd044e53fc710f6e68b2264db8
Pankil <167645528+Pankil02@users.noreply.github.com>
2026-01-24 20:02:33 -0500

    Time: 1 ms (65.39%) | Memory: 43.6 MB (13.49%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,53 +1,13 @@
 class Solution {
-    static class Node{
-	        Node children[]=new Node[26];
-	        int charUsed=0;
-
-	        public Node(){
-	            for(int i=0; i<26; i++){
-	                children[i]=null;
-	            }
-	        }
-	    }
-
-	    public static void insert(String s , Node root){
-	        Node curr =root;
-	        for(int i=0;i<s.length();i++){
-	            int index = s.charAt(i)-'a';
-	            if(curr.children[index]==null){
-	                curr.children[index]=new Node();
-	            }     
-	            curr = curr.children[index];
-	            curr.charUsed++;
-	        }
-	    }
-
-    
     
     public String longestCommonPrefix(String[] strs) {
-        Node root =new Node();
-        int minLen = Integer.MAX_VALUE;
-		for(String st:strs){
-			minLen = Math.min(minLen, st.length());
-            insert(st,root);
-        }
-
         StringBuilder str=new StringBuilder("");
-	        Node curr=root;
-	        int sLength=strs.length;
-	        int i=0;
-	        while(i<minLen){
-	            char c =strs[0].charAt(i);
-	            int indx= c - 'a';
-	            if(curr.children[indx] != null && curr.children[indx].charUsed ==sLength){
-	                str.append(c);
-	            }else{
-	            if(curr.children[indx] != null && curr.children[indx].charUsed !=sLength) break;
-	            }
-	            curr=curr.children[indx];
-	            i++;
-	        }
-	        return str.toString();
-        
+        Arrays.sort(strs);
+        for(int i=0; i<strs[0].length();i++){
+            if(strs[0].charAt(i)==strs[strs.length-1].charAt(i)){
+                str.append(strs[0].charAt(i));
+            }else return str.toString();
+        }
+        return str.toString();
     }
 }
